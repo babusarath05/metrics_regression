7d1df9358afe865bcbdeae7fd98e8f6471843e41
babusarath05 <babusarath05@gmail.com>
2022-03-15 21:44:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,31 +64,6 @@ setup(
 
 
     install_requires=['sklearn','math'],  # Optional
-
-
-
-   
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
-
    
     project_urls={  # Optional
         'Source': 'https://github.com/babusarath05/metrics_regression'
